c41b7260cdf6d150ed5a8fdd512dac391b8c8b4f
Jake Ross <jirhiker@gmail.com>
2021-12-09 15:41:24 -0700

    added polygon to location schema

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pysta",
-    version="0.0.13",
+    version="0.0.14",
     author="Jake Ross",
     description="Python SensorThings Client",
     long_description=long_description,
