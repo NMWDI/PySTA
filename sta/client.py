c0fa844f533875a450b4392c57a86689b3316434
jirhiker <jirhiker@users.noreply.github.com>
2022-01-19 19:11:49 +0000

    Formatting changes

--- sta/client.py
+++ sta/client.py
@@ -356,10 +356,11 @@ class Client:
     def get_thing(self, query=None, name=None, location=None):
         entity = None
         if location:
-            entity = 'Locations({})/Thing'.format(location)
+            entity = "Locations({})/Thing".format(location)
         if name is not None:
             query = "name eq '{}'".format(name)
 
         return next(self.get_things(query, entity))
+
 
 # ============= EOF =============================================
