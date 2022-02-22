b762d5048a8182d3ae0cb13c29bbd1f4ed57d1ac
jakeross <jirhiker@gmail.com>
2022-02-21 17:19:35 -0700

    cleaned up deps

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pysta",
-    version="0.0.27",
+    version="0.0.28",
     author="Jake Ross",
     description="Python SensorThings Client",
     long_description=long_description,
@@ -33,10 +33,8 @@ setup(
     ],
     install_requires=[
         "Click",
-        "paho-mqtt",
-        "pyproj",
         "requests",
-        "jsonschema",
+        "jsonschema==3",
         "pyyaml",
     ],
     # entry_points={
